dffd6ebc2a4a7988aef7b01226c4c909b554157b
DzmitryMiankou <gmiankou@gmail.com>
2023-12-08 11:37:56 +0300

    start

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -15,21 +15,85 @@ const InoutBox = styled.div`
   flex-direction: column;
 `;
 
+interface FurmValue<T extends string> {
+  firstName: T;
+  lastName: T;
+  password: T;
+  email: T;
+}
+
 const Form = () => {
+  const [formValue, setFormValue] = React.useState<FurmValue<string>>({
+    firstName: "",
+    lastName: "",
+    password: "",
+    email: "",
+  });
+
+  const handleRadioChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    event.preventDefault();
+    const target = event.target as HTMLInputElement;
+    switch (target.name) {
+      case "firstName":
+        setFormValue(() => ({ ...formValue, firstName: target.value }));
+        break;
+      case "lastName":
+        setFormValue(() => ({ ...formValue, lastName: target.value }));
+        break;
+      case "email":
+        setFormValue(() => ({ ...formValue, email: target.value }));
+        break;
+      case "password":
+        setFormValue(() => ({ ...formValue, password: target.value }));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <FormBox>
-      <InoutBox>
-        <label htmlFor="firstName">First name:</label>
-        <input id="firstName" name="firstName" type="text" />
-      </InoutBox>
-      <InoutBox>
-        <label htmlFor="lastName">Last name:</label>
-        <input id="lastName" name="lastName" type="text" />
-      </InoutBox>
-      <InoutBox>
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="text" />
-      </InoutBox>
+      <>
+        {[
+          {
+            label: "First name:",
+            idName: "firstName",
+            type: "text",
+            value: formValue.firstName,
+          },
+          {
+            label: "Last name:",
+            idName: "lastName",
+            type: "text",
+            value: formValue.lastName,
+          },
+          {
+            label: "Email:",
+            idName: "email",
+            type: "email",
+            value: formValue.email,
+          },
+          {
+            label: "Password:",
+            idName: "password",
+            type: "password",
+            value: formValue.password,
+          },
+        ].map(({ label, idName, type, value }) => (
+          <InoutBox key={label}>
+            <label htmlFor={idName}>{label}</label>
+            <input
+              id={idName}
+              name={idName}
+              type={type}
+              onChange={handleRadioChange}
+              value={value}
+            />
+          </InoutBox>
+        ))}
+      </>
       <button type="button">Registration</button>
     </FormBox>
   );
